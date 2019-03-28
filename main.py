5272919395bc118ca3705f9086e5cbd0bbaa30c9
Ahmed Hassann <hassanahmed.ah225@gmail.com>
2019-03-28 22:14:37 +0200

    Update main.py
    semi i format

--- main.py
+++ main.py
@@ -21,10 +21,11 @@ class Assembler:
         code_file = open(code_file_path, "w+")
 
         self.content = mips_file.readlines()
-        self.content=self.clean_file()
-        self.data = self.GetInBetween(".data\n",".text\n")
-        self.code = self.GetInBetween(".text\n",".data\n")
-
+        self.clean_file()
+        data_index = self.content.index(".data\n")
+        code_index = self.content.index(".text\n")
+        self.data = self.content[data_index + 1:code_index]
+        self.code = self.content[code_index + 1:]
         self.code_labels = self.get_code_labels()
         self.data_labels = self.get_data_labels()
 
@@ -35,35 +36,8 @@ class Assembler:
         code_file.write(machine_code)
 
         mips_file.close()
-        mips_file.close()
         data_file.close()
         code_file.close()
-    def GetInBetween(self,first,second):
-        data=[]
-        dOffset=-1
-        cOffset=-1
-
-        while True:
-
-            if first in self.content[dOffset+1:len(self.content)]:
-                dOffset=self.content[dOffset+1:len(self.content)].index(first)+dOffset+1
-                if dOffset==-1 :
-                    dOffset=0
-
-                if second in self.content[dOffset + 1:len(self.content)]:
-                    cOffset = self.content[dOffset + 1:len(self.content)].index(second) + dOffset + 1
-                else:
-                    cOffset = len(self.content)
-
-                if first in self.content[dOffset + 1:len(self.content)]:
-                    cOffset = min(cOffset,self.content[dOffset + 1:len(self.content)].index(first) + dOffset + 1)
-
-                data+=self.content[dOffset+1:cOffset]
-            else:
-                break
-
-        return data
-
 
     def clean_file(self):
         output = []
@@ -71,7 +45,7 @@ class Assembler:
             i = i.split("#")[0]
             if re.search(r"\w", i):
                 output.append(i.strip() + "\n")
-        return output
+        self.content = output
 
     def get_data_labels(self):
         labels = {}
@@ -86,11 +60,12 @@ class Assembler:
                     else:
                         labels[match] = line
             match = re.search(r"(\.\w+) (.*)", item)
-            if match:
-                if match[1] == ".word":
-                    line += 4 * len(match[2].split(","))
-                elif match[1] == ".space":
-                    line += 4 * int(match[2])
+
+            if match[1] == ".word":
+                line += 4 * len(match[2].split(","))
+            elif match[1] == ".space":
+                line += 4 * int(match[2])
+
         return labels
 
     def get_code_labels(self):
@@ -112,14 +87,13 @@ class Assembler:
         machine_code = ""
         for i in self.data:
             match = re.search(r"(\.\w+) (.*)", i)
-            if match:
-                if match[1] == ".word":
-                    values = match[2].split(",")
-                    for j in range(0, len(values)):
-                        machine_code += bin(int(values[j])).replace("0b", "").zfill(32) + "\n"
-                elif match[1] == ".space":
-                    for j in range(0, int(match[2])):
-                        machine_code += "".ljust(32, "X") + "\n"
+            if match[1] == ".word":
+                values = match[2].split(",")
+                for j in range(0, len(values)):
+                    machine_code += bin(int(values[j])).replace("0b", "").zfill(32) + "\n"
+            elif match[1] == ".space":
+                for j in range(0, int(match[2])):
+                    machine_code += "".ljust(32, "X") + "\n"
         return machine_code
 
     def assemble_code(self):
@@ -130,9 +104,9 @@ class Assembler:
             "nor": self.nor_assemble,
             "or": self.or_assemble,
             "slt": self.slt_assemble,
-            # "addi": self.addi_assemble,
-            # "lw": self.lw_assemble,
-            # "sw": self.sw_assemble,
+            "lw": self.lw_assemble,
+            "sw": self.sw_assemble,
+            "addi": self.addi_assemble,
             # "beq": self.beq_assemble,
             # "bne": self.bne_assemble,
             # "j" : self.j_assemble,
@@ -155,8 +129,21 @@ class Assembler:
         rd = self.registers.index(str(line[1]).strip())
         rs = self.registers.index(str(line[2]).strip())
         rt = self.registers.index(str(line[3]).strip())
-
         return rs, rt, rd
+
+    def Get_I_Operands(self, instruction_line):
+        line = {}
+        line = re.split(r" |\s*,\s*|[()]", instruction_line)
+        while '' in line: line.remove('')
+        #line = re.search(r'(lw|sw)\s*(\$\w*)\s*,\s*(\w*)[(](\$\w*)[)]\n', instruction_line)
+        if(line[0] == 'addi'):line[3], line[2] = line[2], line[3]
+        dict = self.get_data_labels()
+        if line[2] in dict:
+            line[2] = dict.get(line[2])
+        rs = self.registers.index(str(line[3]).strip())
+        rt = self.registers.index(str(line[1]).strip())
+        im = int(str(line[2]).strip())
+        return rs, rt, im
 
     def R_Machine_Code(self,instruction_line, func):
         rs, rt, rd = self.Get_R_Operands(instruction_line)
@@ -164,6 +151,11 @@ class Assembler:
                  bin(rt).replace("0b", "").zfill(5) + bin(rd).replace("0b", "").zfill(5) + \
                  bin(0).replace("0b", "").zfill(5) + bin(func).replace("0b", "").zfill(6) + "\n"
         return R_machinecode
+
+    def I_Machine_Code(self,instruction_line, func):
+        rs, rt, im = self.Get_I_Operands(instruction_line)
+        I_machinecode = bin(func).replace("0b", "").zfill(6) + bin(rs).replace("0b", "").zfill(5) + bin(rt).replace("0b", "").zfill(5) + bin(im).replace("0b", "").zfill(16) + "\n"
+        return I_machinecode
 
     def add_assemble(self,instruction_line):
         add_machinecode = self.R_Machine_Code(instruction_line, 32)
@@ -188,6 +180,17 @@ class Assembler:
     def slt_assemble(self,instruction_line):
         slt_machinecode = self.R_Machine_Code(instruction_line, 42)
         return slt_machinecode
+
+    def lw_assemble(self,instruction_line):
+        lw_machinecode = self.I_Machine_Code(instruction_line, 35)
+        return lw_machinecode
+
+    def sw_assemble(self,instruction_line):
+        sw_machinecode = self.I_Machine_Code(instruction_line, 43)
+        return sw_machinecode
+
+    def addi_assemble(self,instruction_line):
+        addi_machinecode = self.I_Machine_Code(instruction_line, 8)
 
 
 Assembler("MIPS.asm", "data.txt", "code.txt")
